5d64a0f09071837ce7563e1c40e626197f0ffe04
andrescaro16 <julicaro2003@gmail.com>
2024-09-09 09:24:50 -0500

    feat: add homepage view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Navbar from '@/components/navbar';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <main className='flex min-h-screen flex-col items-center justify-between bg-background p-24'></main>
-    </div>
+    <main className='flex h-[calc(100vh-61px)] flex-col items-center justify-center gap-12 p-12 text-center md:p-24'>
+      <div className='flex flex-col gap-6'>
+        <div>
+          <h1 className='text-5xl font-bold'>¡Hola!</h1>
+          <h1 className='text-5xl font-bold'>Bienvenido a tu entrevista de trabajo</h1>
+        </div>
+        <p className='text-xl font-medium'>
+          Te daremos instrucciones claras y te guiaremos en cada paso. No te preocupes, ¡estamos aquí para ayudarte!
+        </p>
+      </div>
+
+      <div className='w-full'>
+        <Link href='/interview'>
+          <Button className='w-1/2 font-bold'>Comenzar</Button>
+        </Link>
+      </div>
+    </main>
   );
 }
