66625b4a31aa4b4cf3f342f807e16115143308d7
andrescaro16 <julicaro2003@gmail.com>
2024-10-14 19:34:41 -0500

    refactor: abstract services and separate personal details into new page
    Add axios

---
+++ src/app/interview/personal-details/page.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Interview, InterviewSchema } from '@/schemas/personalDetails';
+import { StageHeader } from '@/components/stageHeader';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useTextToSpeechContext } from '@/context/TextToSpeechProvider';
+import { useCVDataContext } from '@/context/CVDataProvider';
+
+const PersonalDetailsPage = () => {
+  const { speak } = useTextToSpeechContext();
+  const { setPersonalDetails } = useCVDataContext();
+
+  const router = useRouter();
+
+  const form = useForm<Interview>({
+    resolver: zodResolver(InterviewSchema),
+    defaultValues: {
+      name: '',
+      phone: '',
+      address: '',
+      email: '',
+    },
+  });
+
+  const onSubmit = async (values: Interview) => {
+    try {
+      setPersonalDetails({
+        name: values.name,
+        phone: values.phone,
+        address: values.address,
+        email: values.email,
+      });
+      speak('Detalles personales guardados');
+      router.push('/success');
+      speak('Sección de experiencia');
+    } catch (_error) {
+      speak('Error al guardar los detalles personales');
+    }
+  };
+
+  return (
+    <div className='flex min-h-screen-minus-nav flex-col gap-5 px-5 pt-10 sm:px-12 md:px-40 xl:px-60 2xl:px-80'>
+      <h1 className='text-2xl font-bold'>¡Empecemos!</h1>
+      <StageHeader
+        step='Paso 1/2'
+        sliderValue={50}
+        description='En este paso, se le harán una serie de preguntas para ayudarle a crear su perfil comenzando con el apartado de información de contacto.'
+      />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='max-w-[448px] space-y-4'>
+          <div className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombre</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Mauricio' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='phone'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Número de teléfono</FormLabel>
+                  <FormControl>
+                    <Input placeholder='3216549870' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Correo electrónico</FormLabel>
+                  <FormControl>
+                    <Input placeholder='mauricio@ejemplo.com' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='address'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Dirección</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Carrera XX, Medellín...' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button aria-label='Botón para dirigirnos hacia la pantalla de obtención de tu experiencia profesional'>
+              Continuar
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default PersonalDetailsPage;
